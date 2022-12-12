695a64510eafc016f4bebcf0e98e538c59df51d6
Alexandr <ragozzzzinalexandr@gmail.com>
2022-12-12 22:24:37 +0500

    1

---
+++ Task25/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Введите число А: ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B: ");
+
+int B = Convert.ToInt32(Console.ReadLine());
+ToDegree(A, B);
+
+
+// Функция возведения в степень
+void ToDegree(int a, int b)
+{
+    int result = 1;
+    for (int i = 1; i <= b; i++)
+    {
+        result = result * a;
+    }
+    Console.WriteLine(result);
+}
